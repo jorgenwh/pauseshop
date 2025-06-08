a2b0d852e59799c71c926140b6be27682f11ee2e
Tarquinen <dannysmo@gmail.com>
2025-06-08 08:55:45 -0400

    tailwind working - validation check with red popup

---
+++ extension/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/popup/popup.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
