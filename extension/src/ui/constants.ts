ad987e64794b052c64152b2b6c8e5452a7208bfa
Tarquinen <dannysmo@gmail.com>
2025-06-18 01:24:19 -0400

    ui cleanup

--- extension/src/ui/constants.ts
+++ extension/src/ui/constants.ts
@@ -2,14 +2,10 @@ export const EXPANDED_SIDEBAR_WIDTH: number = 280;
 export const COMPACT_SIDEBAR_WIDTH: number = 47;
 export const SIDEBAR_SLIDE_DURATION: number = 0.2;
 
-export const SIDEBAR_HEADER_HEIGHT: number = 45;
-export const SIDEBAR_HEADER_HEIGHT_COMPACT: number = 35;
-export const SIDEBAR_HEADER_COMPACT_ICON_SIZE: number = 30;
-export const SIDEBAR_HEADER_EXPANDED_ICON_SIZE: number = 33;
 
 export const UI_CONTAINER_CLASS_NAME: string = "pauseshop-ui-container";
 
 export const NO_PRODUCTS_TIMEOUT_MS: number = 8000;
 
 // Fixed height for compact sidebar when in static states (loading, no products found)
-export const COMPACT_SIDEBAR_STATIC_HEIGHT: number = 95;
+export const COMPACT_SIDEBAR_STATIC_HEIGHT: number = 102;
